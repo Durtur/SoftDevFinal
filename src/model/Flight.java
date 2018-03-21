3f2d197b711367ebed68c620de072139de27e8c2
Símon Örn <simonorn@gmail.com>
2018-03-21 02:13:15 +0000

    Finished programming get Methods for the Database and built a sample Db

--- src/model/Flight.java
+++ src/model/Flight.java
@@ -5,10 +5,83 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author Símon Örn Reynisson <sor7@hi.is>
  */
 public class Flight {
+    private String airline, flightNumber,departureAirport,arrivalAirport;
+    private int price,duration;
+    private Date departureTime,arrivalTime;
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Date getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Date departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Date getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(Date arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+    
+    public String toString(){
+        return airline + " : " + flightNumber + "  " + departureAirport +" -> " + arrivalAirport;
+    }
     
 }
