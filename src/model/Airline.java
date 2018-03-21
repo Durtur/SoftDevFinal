799252bce954ead06127078131feb6d7c27740ee
Símon Örn <simonorn@gmail.com>
2018-03-21 02:13:10 +0000

    Finished programming get Methods for the Database and built a sample Db

--- src/model/Airline.java
+++ src/model/Airline.java
@@ -10,5 +10,26 @@ package model;
  * @author Símon Örn Reynisson <sor7@hi.is>
  */
 public class Airline {
+    private String name;
+    private int priority;
     
+    public Airline(String name, int priority){
+        this.name=name;
+        this.priority=priority;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
