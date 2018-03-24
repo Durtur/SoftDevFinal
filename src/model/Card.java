004bb9de2e6f62f6ef61009de1cd7c6643136cd0
Unnur <uab1@hi.is>
2018-03-24 16:51:23 +0300

    getters and setters for card

--- src/model/Card.java
+++ src/model/Card.java
@@ -7,8 +7,43 @@ package model;
 
 /**
  *
- * @author Símon Örn Reynisson <sor7@hi.is>
+ * @author Unnur Bjarnadóttir <uab1@hi.is>
  */
 public class Card {
+    private String fullName, expiryDate;
+    private long cardNumber;
+    private int securityNumber;
     
+    public String getFullName() {
+        return fullName;
+    }
+    
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+    
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+    
+    public long getCardNumber() {
+        return cardNumber;
+    }
+    
+    public void setCardNumber(long cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+    
+    public int getSecurityNumber() {
+        return securityNumber;
+    }
+    
+    public void setSecurityNumber(int securityNumber) {
+        this.securityNumber = securityNumber;
+    }
 }
+
