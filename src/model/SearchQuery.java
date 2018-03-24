cb0f5e39d3175cea37c2ac4118121ff6eec95a1c
Unnur <uab1@hi.is>
2018-03-24 20:16:26 +0300

    Add getters and setters to SearchQuery

--- src/model/SearchQuery.java
+++ src/model/SearchQuery.java
@@ -5,10 +5,71 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author Símon Örn Reynisson <sor7@hi.is>
  */
 public class SearchQuery {
+    private Date firstDate, secondDate;
+    private String departingFrom, arrivingTo, airline;
+    private int passengerNo;
     
+    public SearchQuery(Date firstDate, Date secondDate, String departingFrom, String arrivingTo, String airline, int passengerNo) {
+    	this.firstDate = firstDate;
+    	this.secondDate = secondDate;
+    	this.departingFrom = departingFrom;
+    	this.arrivingTo = arrivingTo;
+    	this.airline = airline;
+    	this.passengerNo = passengerNo;
+    }
+    
+    public Date getFirstDate() {
+    	return firstDate;
+    }
+    
+    public void setFirstDate(Date firstDate) {
+    	this.firstDate = firstDate;
+    }
+    
+    public Date getSecondDate() {
+    	return secondDate;
+    }
+    
+    public void setSecondDate(Date secondDate) {
+    	this.secondDate = secondDate;
+    }
+    
+    public String getDepartingFrom() {
+    	return departingFrom;
+    }
+    
+    public void setDepartingFrom(String departingFrom) {
+    	this.departingFrom = departingFrom;
+    }
+    
+    public String getArrivingTo() {
+    	return arrivingTo;
+    }
+    
+    public void setArrivingTo(String arrivingTo) {
+    	this.arrivingTo = arrivingTo;
+    }
+    
+    public String getAirline() {
+    	return airline;
+    }
+    
+    public void setAirline(String airline) {
+    	this.airline = airline;
+    }
+    
+    public int getPassengerNo() {
+    	return passengerNo;
+    }
+    
+    public void setPassengerNo(int passengerNo) {
+    	this.passengerNo = passengerNo;
+    }
 }
