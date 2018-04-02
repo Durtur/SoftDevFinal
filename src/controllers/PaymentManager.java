f914eca2f4fbf73b633e89fc4f3f110451c3c97d
Unnur <uab1@hi.is>
2018-04-02 16:08:51 +0300

    Method in PaymentManager

--- src/controllers/PaymentManager.java
+++ src/controllers/PaymentManager.java
@@ -5,10 +5,27 @@
  */
 package controllers;
 
+import java.util.Objects;
+import model.Card;
+
 /**
  *
  * @author Símon Örn Reynisson <sor7@hi.is>
  */
 public class PaymentManager {
     
+    public boolean processPayment(Card cardInfo) {
+        boolean success = false;
+        String cardNumberString;
+        cardNumberString = Long.toString(cardInfo.getCardNumber());
+        int securityNum;
+        securityNum = cardInfo.getSecurityNumber();
+        if (Objects.equals(cardInfo.getFullName(), "") && 
+                Objects.equals(cardInfo.getExpiryDate(), "") && 
+                cardNumberString.length() == 16 && 
+                securityNum == 3) {
+            success = true;
+        }
+        return success;
+    }
 }
