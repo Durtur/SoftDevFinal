ff8ee94d87f4a33b99e95057a94f296bf846f169
Unnur <uab1@hi.is>
2018-04-02 16:16:25 +0300

    Method in PaymentManager

--- src/controllers/PaymentManager.java
+++ src/controllers/PaymentManager.java
@@ -14,6 +14,7 @@ import model.Card;
  */
 public class PaymentManager {
     
+    // Checks if inputs into Card are valid (to a certain extent)
     public boolean processPayment(Card cardInfo) {
         boolean success = false;
         String cardNumberString;
