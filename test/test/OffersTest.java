302889fa899e39027828d9974b5d31c0e7075711
Símon Örn <simonorn@gmail.com>
2018-04-03 19:32:46 +0000

    Tester class

--- test/test/OffersTest.java
+++ test/test/OffersTest.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
-/**
- *
- * @author Símon Örn Reynisson <sor7@hi.is>
- */
+import controllers.OfferManager;
+import database.DatabaseManager;
+import java.util.ArrayList;
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class OffersTest {
-    
+
+    OffersMock om;
+
+    @Before
+    public void setUp() {
+        ArrayList<String> offers = new ArrayList<String>();
+        om = new OffersMock();
+    }
+
+    @After
+    public void tearDown() {
+        ArrayList<String> offers = null;
+        om = null;
+    }
+
+    @Test //when there's offers
+    public void offersMockTest() {
+        ArrayList<String[]> offers = om.getOffers();
+        assertTrue(offers.size() != 0);
+    }
+
+    @Test //when there's no offers
+    public void noOffersMockTest() {
+        ArrayList<String[]> offers = om.noOffers();
+        assertNotNull(offers);
+        assertEquals(0, offers.size());
+    }
+
+    @Test //when connection to db fails
+    public void noConnectionMockTest() {
+        ArrayList<String[]> offers = om.noDBConnection();
+        assertTrue(offers == null);
+
+    }
 }
