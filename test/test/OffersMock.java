c48d44f9c27f9b3d00c2b879b994eb5c3306d6bf
Símon Örn <simonorn@gmail.com>
2018-04-03 19:34:52 +0000

    Tester class

---
+++ test/test/OffersMock.java
@@ -0,0 +1,38 @@
+package test;
+
+import java.util.ArrayList;
+
+public class OffersMock {
+
+    private ArrayList<String[]> offers;
+
+    private void setUp() {
+        offers = new ArrayList();
+        String headerString = "a";
+        int pathNum = 0;
+        for (int i = 0; i < 10; i++) {
+            headerString += "a";
+            pathNum++;
+            offers.add(new String[]{headerString, "offer" + pathNum + ".jgp"});
+        }
+    }
+    
+    //simulates the situation where there are offers available
+    public ArrayList<String[]> getOffers() {
+        return offers;
+    }
+    
+    //simulates the situation where there are no offers
+    public ArrayList<String[]> noOffers() {
+        return new ArrayList();
+    }
+    
+    
+    //simulates the situation where the db cannot be accessed
+    public ArrayList<String[]> noDBConnection(){
+        return null;
+    }
+}
+
+
+
