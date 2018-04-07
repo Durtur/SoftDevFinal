13d2be6ea7dfc3a4547ff7431d8d0b401c1674d1
Unnur <uab1@hi.is>
2018-04-08 02:07:26 +0300

    Classes for disallowing numbers or letters when needed. Validation of input in PaymentPage

--- src/controllers/PaymentPageController.java
+++ src/controllers/PaymentPageController.java
@@ -11,6 +11,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
@@ -65,8 +67,48 @@ public class PaymentPageController implements Initializable {
         String card = cardNumberInput.getText();
         String expiry = expiryDateInput.getText();
         String security = securityNumberInput.getText();
-        System.out.println("Name: " + name + ". Ssn: " + ssn + ". CheckBags: " + cb1 + ". CarryBags: " + cb2
-                            + ". Card: " + card + ". Expiry: " + expiry + ". Security: " + security);
+        
+        if (validateCard(card) != true) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setHeaderText(null);
+            alert.setTitle("Invalid card number");
+            alert.setContentText("Please enter valid card number");
+            alert.show();
+        }
+        
+        if (validateSecurity(security) != true) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setHeaderText(null);
+            alert.setTitle("Invalid security number");
+            alert.setContentText("Please enter valid security number");
+            alert.show();
+        }
+        
+        if (validateCheckBags(cb1) != true) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setHeaderText(null);
+            //alert.setTitle("Please select check-in bags");
+            alert.setContentText("Please select check-in bags");
+            alert.show();
+        }
+        
+        if (validateCarryBags(cb2) != true) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setHeaderText(null);
+            //alert.setTitle("Please select carry-on bags");
+            alert.setContentText("Please select carry-on bags");
+            alert.show();
+        }
+        
+        if (validateSsn(ssn) != true) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setHeaderText(null);
+            alert.setTitle("Invalid ssn");
+            alert.setContentText("Please enter valid ssn");
+            alert.show();
+        }
+        
+        
     }
     
     private void populateBoxes() {
@@ -84,6 +126,36 @@ public class PaymentPageController implements Initializable {
     @FXML
     public void setFlightInfo(String currentFlight) {
         flightInfo.setText(currentFlight);
+    }
+    
+    private boolean validateCard(String card) {
+        boolean correct;
+        correct = cardNumberInput.getText().length() == 16;
+        return correct;
+    }
+    
+    private boolean validateSecurity(String security) {
+        boolean correct;
+        correct = securityNumberInput.getText().length() == 3;
+        return correct;
+    }
+    
+    private boolean validateCheckBags(String checkBags) {
+        boolean correct;
+        correct = checkedBagsInput.getValue() != null;
+        return correct;
+    }
+    
+    private boolean validateCarryBags(String carryBags) {
+        boolean correct;
+        correct = carryOnBagsInput.getValue() != null;
+        return correct;
+    }
+    
+    private boolean validateSsn(String ssn) {
+        boolean correct;
+        correct = ssnInput.getText().length() == 10;
+        return correct;
     }
     
 }
---
+++ src/utils/FullNameTextField.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author unnur
+ */
+public class FullNameTextField extends TextField {
+    
+    @Override
+    public void replaceText(int i, int i1, String string) {
+        if (string.matches("[a-zA-Z]")) {
+            super.replaceText(i, i1, string);
+        }
+    }
+    
+    @Override
+    public void replaceSelection(String string) {
+        super.replaceSelection(string);
+    }
+}
